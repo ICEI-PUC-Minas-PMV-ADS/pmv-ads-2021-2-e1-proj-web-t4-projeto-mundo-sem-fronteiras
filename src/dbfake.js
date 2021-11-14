5933dcec19be98ca879afc7239490c797cdfe2e9
TiagoMiquéias <91267781+TiagoMiqueiasDesenvolvedor@users.noreply.github.com>
2021-11-14 01:53:07 -0300

    Create dbfake.js

---
+++ src/dbfake.js
@@ -0,0 +1,33 @@
+var dv = {
+    dados : 
+    [
+        {
+            paisResumo: 'Portugal',
+            Continente: 'Europeu',
+            documentacao: 'Passaporte válido, Seguro de viagem internacional com cobertura para toda  a sua estadia no país ou PB4, Passagens de ida e volta, Comprovante de onde ficará hospedado, Comprovante que tem condições financeiras de se manter no pais durante a viagem, Teste negativo para Covid',
+            recomendacoes: 'Torre de Belem, Parque e Palacio Nacional da Pena, Castelo de São. Jorge, Universidade de Coimbra',
+            orcamento: ' mil e quinhentos euros',
+         }, 
+        {
+            paisResumo: 'Alemanha',
+            Continente: 'Europeu',
+            documentacao: 'Passaporte válido, Seguro de viagem internacional com cobertura para toda  a sua estadia no país ou PB4, Passagens de ida e volta, Comprovante de onde ficará hospedado, Comprovante que tem condições financeiras de se manter no pais durante a viagem, Teste negativo para Covid',
+            recomendacoes: '',
+            orcamento: ' três e quinhentos euros',
+        }, 
+        {
+            paisResumo: 'Estados Unidos',
+            Continente: 'Americano',
+            documentacao: 'Passaporte válido, Seguro de viagem internacional com cobertura para toda  a sua estadia no país ou PB4, Passagens de ida e volta, Comprovante de onde ficará hospedado, Comprovante que tem condições financeiras de se manter no pais durante a viagem, Teste negativo para Covid',
+            recomendacoes: 'Estátua da Liberdade', 'Little Havana',
+            orcamento: ' 5 mil dólares',
+        }, 
+        {
+            paisResumo: 'Australia',
+            Continente: 'Europeu',
+            documentacao: 'Passaporte válido, Seguro de viagem internacional com cobertura para toda  a sua estadia no país ou PB4, Passagens de ida e volta, Comprovante de onde ficará hospedado, Comprovante que tem condições financeiras de se manter no pais durante a viagem, Teste negativo para Covid',
+            recomendacoes: '',
+            orcamento: ' 3 mil e dólares australianos',
+        },
+    ]
+} 
