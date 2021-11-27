c14b3f490913653bd9e912716bf229121745d7dc
TiagoMiquéias <91267781+TiagoMiqueiasDesenvolvedor@users.noreply.github.com>
2021-11-27 20:38:22 -0300

    Alemanha.hml

---
+++ src/dbfake.js
@@ -0,0 +1,14 @@
+var db =
+{
+    dados: 
+    [
+        {
+            titulo: 'Alemanha',
+            sobre: " é um país situado na Europa Ocidental com uma paisagem de florestas, rios, cordilheiras e praias do Mar do Norte. A nação tem mais de 2 milênios de história. Berlim, a capital, abriga cenários artísticos e de vida noturna, o Portão de Brandemburgo e muitos locais relacionados à Segunda Guerra Mundial. Munique é conhecida pela Oktoberfest e pelos beer halls, entre eles o Hofbräuhaus, do século XVI. Frankfurt, com seus arranha-céus, abriga o Banco Central Europeu.",
+            sobredois: "Com 83,1 milhões de habitantes, a Alemanha é o país mais populoso da UE e um dos países com a maior densidade demográfica. Cerca de 77% dos habitantes vivem em áreas com média ou alta densidade demográfica. Cerca de 30% da população vivem em metrópoles com mais de 100 mil habitantes, no total de 80 no país, entre elas quatro cidades com mais de um milhão de habitantes (Berlim, Hamburg, Munique, Colônia). ",
+            iPassaport: "Documento padrão necessário para quase todas as viagens internacionais partindo do Brasil, com exceção dos países da MERCOSUL, onde você pode utilizar sua identidade brasileira como ducumentação. Podendo ser consultadas as regras para solicitação do passaporte, no link das",
+            iVisto: "Visto não é necessário para viagens de até 90 dias. Desde que cidadãos brasileiros portem passaportes válidos comuns, diplomáticos, de serviço ou outros passaportes oficiais podem entrar ou passar em transito na Alemanha e nos países membros da União Europeia (exceto no Reino Unido e Irlanda). O processo pode ser consultado no site das",
+            iCarteiraDeVacinacao: "Para a entrada na Alemanha, depois da crise causada pela SARS-CoV-2 (COVID-19), é necessário apresentar um certificado digital COVID da UE ou comprovante de vacinação comparável, digital ou em papel, em idioma alemão, inglês, francês, italiano ou espanhol. Fonte oficial em:",
+        }
+    ]
+}
